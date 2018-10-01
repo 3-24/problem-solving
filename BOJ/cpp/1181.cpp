656a48dcf95428548ad35a8490a2c43d709ada4b
Youngseok <yschoelife@gmail.com>
2018-10-01 10:37:39 +0900

    compare BOJ

---
+++ BOJ/cpp/1181.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int compare(const void *a, const void *b){
+  int p = strlen((char *)a),q = strlen((char *)b);
+  if (p != q){
+    return p-q;
+  }
+  else return strcmp((char *)a, (char *)b);
+}
+
+int main(){
+  int n;
+  scanf("%d",&n);
+  char words[n][50];
+  for (int i=0;i<n;i++){
+    scanf("%s",words[i]);
+  }
+  qsort(words,n,sizeof(words[0]),compare);
+  int i=0,j;
+  while (i < n){
+    printf("%s\n",words[i]);
+    j = i;
+    do{i++;}
+    while (strcmp(words[i],words[j])==0);
+  }
+}
