d98d3a146dad81270a4087906fd893b6ed1a3575
Youngseok <yschoelife@gmail.com>
2018-10-08 12:50:08 +0900

    gitignore

--- BOJ/cpp/temp.cpp
+++ BOJ/cpp/temp.cpp
@@ -1,8 +1,13 @@
-#include <bits/stdc++.h>
+#include <stdio.h>
+#include <stack>
+using namespace std;
 
 int main(){
-  int d=100;
-  if (d){
-    printf("True");
+  int a[10];
+  for (int i=0;i<10;i++){
+    a[i] = i;
   }
+  int (*p)[10] = &a;
+  //printf("%d",(*p)[3]);
+  printf("%d",*p[3]);
 }
