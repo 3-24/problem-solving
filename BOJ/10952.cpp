78a9832980ddaf3e59fda7a9122a42ac7775e65d
3-24 <dev.youngseok@gmail.com>
2019-07-29 04:17:58 +0900

    ez

---
+++ BOJ/10952.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int a,b;
+    while(true){
+        cin >> a >> b;
+        if (a ==0 && b ==0) break;
+        cout << a+b << endl;
+    }
+    return 0;
+}
