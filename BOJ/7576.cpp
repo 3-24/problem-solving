b106d88064b3838f047550211e94e747abf4b17a
3-24 <yschoelife@gmail.com>
2019-07-30 17:21:30 +0900

    searching problem

--- BOJ/7576.cpp
+++ BOJ/7576.cpp
@@ -3,45 +3,62 @@
 using namespace std;
 
 const int MAX_SIZE = 1000;
+int n,m;
+int stat[MAX_SIZE + 1][MAX_SIZE + 1], days[MAX_SIZE + 1][MAX_SIZE + 1] = {0,};
+bool visited[MAX_SIZE + 1][MAX_SIZE + 1] = {false,};
 
 pair<int,int> operator + (pair<int,int> a, pair<int,int> b){
-
+    return make_pair(a.first + b.first, a.second + b.second);
 }
 
-int main(){
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+bool validPair(pair<int, int> x){
+    return (0 <= x.first && x.first < n && 0 <= x.second && x.second < m );
+}
 
-    int n,m, stat[MAX_SIZE+1][MAX_SIZE+1], day[MAX_SIZE+1][MAX_SIZE+1] = {0,};
-    bool visited[MAX_SIZE+1][MAX_SIZE+1] = {false,};
+int main() {
 
-    cin >> n >> m;
+    cin >> m >> n;
 
-    list <pair<int,int>> p,q;
-    int count = 0, empty = 0;
+    list<pair<int, int>> q;
+    int max_days = 0, empty = 0, count = 0;
 
-
-    for (int i=0;i<n;i++){
-        for (int j=0;j<m;j++){
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
             int t;
             cin >> t;
             stat[i][j] = t;
-            if (t == 1){
-                q.push_back(make_pair(i,j));
-            }
-            else if (t == -1) empty++;
+            if (t == 1) {
+                q.push_back(make_pair(i, j));
+                visited[i][j] = true;
+                count++;
+            } else if (t == -1) empty++;
         }
     }
 
-    const pair<int,int> ways[4] = {make_pair(1,0), make_pair(-1,0), make_pair(0,1), make_pair(0,-1)};
+    int tomato_n = n * m - empty;
 
-    while(!q.empty()){
-        pair<int,int> s = q.front();
+    pair<int, int> ways[4] = {make_pair(1, 0), make_pair(-1, 0), make_pair(0, 1), make_pair(0, -1)};
+
+    while (!q.empty()) {
+        pair<int, int> s = q.front();
         q.pop_front();
-        if (!visited[s.first][s.second]){
-            visited[s.first][s.second] = true;
-        }
 
+        for (int i = 0; i < 4; i++) {
+            pair<int, int> d = s + ways[i];
+
+            if (validPair(d) && !visited[d.first][d.second] && stat[d.first][d.second] != -1) {
+                visited[d.first][d.second] = true;
+                count++;
+                q.push_back(d);
+                days[d.first][d.second] = days[s.first][s.second] + 1;
+                max_days = max(max_days, days[d.first][d.second]);
+            }
+        }
     }
 
+    if (count == tomato_n) {
+        cout << max_days << endl;
+    } else cout << -1 << endl;
+
+    return 0;
 }
