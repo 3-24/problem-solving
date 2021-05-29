d3df1d9d1dc9a53ee2113d9e5b56467a5fce48bd
Youngseok Choe <dev.youngseok@gmail.com>
2021-05-29 23:08:17 +0900

    Upcode Round 633 Div 2 Problem C

--- codeforces/r633d2/c.cpp
+++ codeforces/r633d2/c.cpp
@@ -2,18 +2,6 @@
 using namespace std;
 int arr[100001];
 
-int calc(int n, int i){
-    if (i >= n-1) return 0;
-    if (arr[i] <= arr[i+1]){        // i <= n-2
-        return calc(n, i+1);
-    }
-    else{ // arr[i] > arr[i+1]
-        int c = arr[i] - arr[i+1];
-        arr[i+1] = arr[i];
-        return max(calc(n, i+1), c);
-    }
-}
-
 int main()
 {
     ios_base::sync_with_stdio(false);
@@ -26,14 +14,22 @@ int main()
         for (int i=0;i<n;i++){
             cin >> arr[i];
         }
-        int result = calc(n,0);
+        int result = 0;
+
+        for (int i=0;i<n-1;i++){
+            int diff = arr[i] - arr[i+1];
+            if (arr[i] > arr[i+1]){
+                if (result < diff) result = diff;
+                arr[i+1] = arr[i];
+            }
+        }
+
         int i=0;
         while (1){
             if (result+1 <= (1 << i)) break;
             i++;
         }
         cout << i << '\n';
-        
     }
     return 0;
 }
