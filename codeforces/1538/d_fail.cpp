1da36c3e173bce21799df5304c4a9f956232a254
Youngseok Choe <dev.youngseok@gmail.com>
2021-06-11 02:30:40 +0900

    Finish CF Round #725 Div.3

---
+++ codeforces/1538/d_fail.cpp
@@ -0,0 +1,133 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+
+#ifndef ONLINE_JUDGE
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+
+#define faster  ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define all(x) x.begin(), x.end()
+#define sum(a)     ( accumulate ((a).begin(), (a).end(), 0ll))
+#define mine(a)    (*min_element((a).begin(), (a).end()))
+#define maxe(a)    (*max_element((a).begin(), (a).end()))
+#define mini(a)    ( min_element((a).begin(), (a).end()) - (a).begin())
+#define maxi(a)    ( max_element((a).begin(), (a).end()) - (a).begin())
+#define lowb(a, x) ( lower_bound((a).begin(), (a).end(), (x)) - (a).begin())
+#define uppb(a, x) ( upper_bound((a).begin(), (a).end(), (x)) - (a).begin())
+
+#define mp   make_pair
+#define pb   push_back
+#define eb   emplace_back
+
+template<typename T>             vector<T>& operator--            (vector<T> &v){for (auto& i : v) --i;            return  v;}
+template<typename T>             vector<T>& operator++            (vector<T> &v){for (auto& i : v) ++i;            return  v;}
+template<typename T>             istream& operator>>(istream& is,  vector<T> &v){for (auto& i : v) is >> i;        return is;}
+template<typename T>             ostream& operator<<(ostream& os,  vector<T>  v){for (auto& i : v) os << i << ' '; return os;}
+template<typename T, typename U> pair<T,U>& operator--           (pair<T, U> &p){--p.first; --p.second;            return  p;}
+template<typename T, typename U> pair<T,U>& operator++           (pair<T, U> &p){++p.first; ++p.second;            return  p;}
+template<typename T, typename U> istream& operator>>(istream& is, pair<T, U> &p){is >> p.first >> p.second;        return is;}
+template<typename T, typename U> ostream& operator<<(ostream& os, pair<T, U>  p){os << p.first << ' ' << p.second; return os;}
+template<typename T, typename U> pair<T,U> operator-(pair<T,U> a, pair<T,U> b){return mp(a.first-b.first, a.second-b.second);}
+template<typename T, typename U> pair<T,U> operator+(pair<T,U> a, pair<T,U> b){return mp(a.first+b.first, a.second+b.second);}
+template<typename T, typename U> void umin(T& a, U b){if (a > b) a = b;}
+template<typename T, typename U> void umax(T& a, U b){if (a < b) a = b;}
+template<typename T>
+bool exist_vector(vector<T> v, T x){return (find(v.begin(), v.end(), x) != v.end()) ? true : false;} // unsorted
+
+ll count_prime_factor(ll n, ll goal){
+    ll count = 0;
+    while (n % 2 == 0){
+        count++;
+        if (count == goal){
+            return count;
+        }
+        n = n/2;
+    }
+
+    for (ll i=3; i<=sqrt(n); i++){
+        while (n % i == 0){
+            count++;
+            if (count == goal) return count;
+            n = n/i;
+        }
+    }
+
+    if (n >= 2) count++;
+    return count;
+}
+
+void solve(){
+    ll a,b,k;
+    cin >> a >> b >> k;
+    if (a>b) swap(a,b);
+    if (a == 1 && b == 1){
+        cout << "No" << endl;
+        return;
+    }
+    if (a == 1){
+        if (count_prime_factor(b, k) == k) cout << "Yes" << endl;
+        else cout << "No" << endl;
+        return;
+    }
+
+    if (k == 1){
+        if (b % a == 0 && a != b) cout << "Yes" << endl;
+        else cout << "No" << endl;
+        return;
+    }
+    else {
+        ll v = count_prime_factor(a, k-1);
+        if (v == k-1){
+            cout << "Yes" << endl;
+            return;
+        }
+        ll x = count_prime_factor(b, k-v);
+        if (x == k-v){
+            cout << "Yes" << endl;
+        }
+        else cout << "No" << endl;
+    }
+
+    return;
+}
+
+int main(){
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#else
+#endif
+    faster
+    ll t;
+    cin >> t;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
