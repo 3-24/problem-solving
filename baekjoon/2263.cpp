d22f625d1cc7e7bfa778b3314d19d43a5ff9e6a0
Youngseok Choe <dev.youngseok@gmail.com>
2021-08-19 04:27:28 +0900

    Solve acmicpc.net 2263 '트리의 순회'

---
+++ baekjoon/2263.cpp
@@ -0,0 +1,107 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+template<typename T>
+void _print_arr(const T x[], int size) {cerr << '{' ; for (int i=0; i<size; i++) cerr << x[i] << ((i == size-1) ? "" : ","); cerr << "}]" << '\n'; }
+
+
+#ifndef ONLINE_JUDGE
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#define debug_arr(x,y) cerr << "[" << #x << "] = ["; _print_arr(x, y)
+#else
+#define debug(x...)
+#define debug_arr(x, y)
+#endif
+
+#define faster  ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define all(x) x.begin(), x.end()
+#define sum(a)     ( accumulate ((a).begin(), (a).end(), 0ll))
+#define mine(a)    (*min_element((a).begin(), (a).end()))
+#define maxe(a)    (*max_element((a).begin(), (a).end()))
+#define mini(a)    ( min_element((a).begin(), (a).end()) - (a).begin())
+#define maxi(a)    ( max_element((a).begin(), (a).end()) - (a).begin())
+#define lowb(a, x) ( lower_bound((a).begin(), (a).end(), (x)) - (a).begin())
+#define uppb(a, x) ( upper_bound((a).begin(), (a).end(), (x)) - (a).begin())
+
+#define mp   make_pair
+#define pb   push_back
+#define eb   emplace_back
+
+template<typename T>             vector<T>& operator--            (vector<T> &v){for (auto& i : v) --i;            return  v;}
+template<typename T>             vector<T>& operator++            (vector<T> &v){for (auto& i : v) ++i;            return  v;}
+template<typename T>             istream& operator>>(istream& is,  vector<T> &v){for (auto& i : v) is >> i;        return is;}
+template<typename T>             ostream& operator<<(ostream& os,  vector<T>  v){for (auto& i : v) os << i << ' '; return os;}
+template<typename T, typename U> pair<T,U>& operator--           (pair<T, U> &p){--p.first; --p.second;            return  p;}
+template<typename T, typename U> pair<T,U>& operator++           (pair<T, U> &p){++p.first; ++p.second;            return  p;}
+template<typename T, typename U> istream& operator>>(istream& is, pair<T, U> &p){is >> p.first >> p.second;        return is;}
+template<typename T, typename U> ostream& operator<<(ostream& os, pair<T, U>  p){os << p.first << ' ' << p.second; return os;}
+template<typename T, typename U> pair<T,U> operator-(pair<T,U> a, pair<T,U> b){return mp(a.first-b.first, a.second-b.second);}
+template<typename T, typename U> pair<T,U> operator+(pair<T,U> a, pair<T,U> b){return mp(a.first+b.first, a.second+b.second);}
+template<typename T, typename U> void umin(T& a, U b){if (a > b) a = b;}
+template<typename T, typename U> void umax(T& a, U b){if (a < b) a = b;}
+template<typename T>
+bool exist_vector(vector<T> v, T x){return (find(v.begin(), v.end(), x) != v.end()) ? true : false;} // unsorted
+
+int inorder[100001];
+int inorder_reverse[100001];
+int postorder[100001];
+int postorder_reverse[100001];
+
+void preorder_unit(int inorder_start, int inorder_end, int postorder_start, int postorder_end){
+    debug(inorder_start, inorder_end, postorder_start, postorder_end);
+    if (postorder_start == postorder_end) return;
+    int x = postorder[postorder_end-1];
+    int x_ind = inorder_reverse[x];
+    debug (x, x_ind);
+    int left_tree_size = x_ind - inorder_start;
+    int right_tree_size = inorder_end  - x_ind -1;
+    cout << x << ' ';
+    if (left_tree_size > 0) preorder_unit(inorder_start, x_ind, postorder_start, postorder_start+left_tree_size);
+    if (right_tree_size > 0) preorder_unit(x_ind+1, inorder_end, postorder_start+left_tree_size, postorder_end-1);
+}
+
+int main(){
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#else
+#endif
+    faster
+    int n;
+    cin >> n;
+    for (int i=1; i<n+1; i++){
+        cin >> inorder[i];
+        inorder_reverse[inorder[i]] = i;
+    }
+    for (int i=1; i<n+1; i++){
+        cin >> postorder[i];
+        postorder_reverse[postorder[i]] = i;
+    }
+
+    preorder_unit(1, n+1, 1, n+1);
+
+    return 0;
+}
