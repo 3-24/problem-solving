aa5e2980bf964a82a7232a4f0cd15756ff27e616
3-24 <dev.youngseok@gmail.com>
2021-08-21 23:20:36 +0900

    Make template.cpp more simple

--- baekjoon/template.cpp
+++ baekjoon/template.cpp
@@ -24,16 +24,11 @@ void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ 
 void _print() {cerr << "]\n";}
 template <typename T, typename... V>
 void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
-template<typename T>
-void _print_arr(const T x[], int size) {cerr << '{' ; for (int i=0; i<size; i++) cerr << x[i] << ((i == size-1) ? "" : ","); cerr << "}]" << '\n'; }
-
 
 #ifndef ONLINE_JUDGE
 #define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
-#define debug_arr(x,y) cerr << "[" << #x << "] = ["; _print_arr(x, y)
 #else
 #define debug(x...)
-#define debug_arr(x, y)
 #endif
 
 #define faster  ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
@@ -45,10 +40,6 @@ void _print_arr(const T x[], int size) {cerr << '{' ; for (int i=0; i<size; i++)
 #define maxi(a)    ( max_element((a).begin(), (a).end()) - (a).begin())
 #define lowb(a, x) ( lower_bound((a).begin(), (a).end(), (x)) - (a).begin())
 #define uppb(a, x) ( upper_bound((a).begin(), (a).end(), (x)) - (a).begin())
-
-#define mp   make_pair
-#define pb   push_back
-#define eb   emplace_back
 
 template<typename T>             vector<T>& operator--            (vector<T> &v){for (auto& i : v) --i;            return  v;}
 template<typename T>             vector<T>& operator++            (vector<T> &v){for (auto& i : v) ++i;            return  v;}
@@ -62,8 +53,6 @@ template<typename T, typename U> pair<T,U> operator-(pair<T,U> a, pair<T,U> b){r
 template<typename T, typename U> pair<T,U> operator+(pair<T,U> a, pair<T,U> b){return mp(a.first+b.first, a.second+b.second);}
 template<typename T, typename U> void umin(T& a, U b){if (a > b) a = b;}
 template<typename T, typename U> void umax(T& a, U b){if (a < b) a = b;}
-template<typename T>
-bool exist_vector(vector<T> v, T x){return (find(v.begin(), v.end(), x) != v.end()) ? true : false;} // unsorted
 
 int main(){
 #ifndef ONLINE_JUDGE
