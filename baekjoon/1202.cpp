b358b101cd20d7aca296627b6376469a48691ec3
Youngseok Choe <dev.youngseok@gmail.com>
2021-08-25 22:33:30 +0900

    Solve acmicpc.net 1202 ''

---
+++ baekjoon/1202.cpp
@@ -0,0 +1,110 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long int ll;
+
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+
+#ifndef ONLINE_JUDGE
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+
+#define faster  ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define all(x) x.begin(), x.end()
+#define sum(a)     ( accumulate ((a).begin(), (a).end(), 0ll))
+#define mine(a)    (*min_element((a).begin(), (a).end()))
+#define maxe(a)    (*max_element((a).begin(), (a).end()))
+#define mini(a)    ( min_element((a).begin(), (a).end()) - (a).begin())
+#define maxi(a)    ( max_element((a).begin(), (a).end()) - (a).begin())
+#define lowb(a, x) ( lower_bound((a).begin(), (a).end(), (x)) - (a).begin())
+#define uppb(a, x) ( upper_bound((a).begin(), (a).end(), (x)) - (a).begin())
+
+template<typename T>             vector<T>& operator--            (vector<T> &v){for (auto& i : v) --i;            return  v;}
+template<typename T>             vector<T>& operator++            (vector<T> &v){for (auto& i : v) ++i;            return  v;}
+template<typename T>             istream& operator>>(istream& is,  vector<T> &v){for (auto& i : v) is >> i;        return is;}
+template<typename T>             ostream& operator<<(ostream& os,  vector<T>  v){for (auto& i : v) os << i << ' '; return os;}
+template<typename T, typename U> pair<T,U>& operator--           (pair<T, U> &p){--p.first; --p.second;            return  p;}
+template<typename T, typename U> pair<T,U>& operator++           (pair<T, U> &p){++p.first; ++p.second;            return  p;}
+template<typename T, typename U> istream& operator>>(istream& is, pair<T, U> &p){is >> p.first >> p.second;        return is;}
+template<typename T, typename U> ostream& operator<<(ostream& os, pair<T, U>  p){os << p.first << ' ' << p.second; return os;}
+template<typename T, typename U> pair<T,U> operator-(pair<T,U> a, pair<T,U> b){return mp(a.first-b.first, a.second-b.second);}
+template<typename T, typename U> pair<T,U> operator+(pair<T,U> a, pair<T,U> b){return mp(a.first+b.first, a.second+b.second);}
+template<typename T, typename U> void umin(T& a, U b){if (a > b) a = b;}
+template<typename T, typename U> void umax(T& a, U b){if (a < b) a = b;}
+
+struct compare {
+    bool operator()(pair<int, int> a, pair<int, int> b){
+        return a.second < b.second;
+    }
+};
+
+int main(){
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#else
+#endif
+    faster
+    int n, k;
+    cin >> n >> k;
+
+    priority_queue<pair<int ,int>, vector<pair<int, int>>, compare> jewels;
+    queue<pair<int, int>> all_jewels;
+    multiset<int> bags;
+
+    while(n--){
+        int weight, value;
+        cin >> weight >> value;
+        all_jewels.push({weight, value});
+    }
+
+    int max_bag_size = 0;
+    while (k--){
+        int bag_size;
+        cin >> bag_size;
+        bags.insert(bag_size);
+        max_bag_size = max(max_bag_size, bag_size);
+    }
+
+    while(!all_jewels.empty()){
+        auto p = all_jewels.front();
+        all_jewels.pop();
+        if (p.first <= max_bag_size) jewels.push(p);
+    }
+
+    ll sum = 0;
+    while(!jewels.empty()){
+        auto [weight, value] = jewels.top();
+        jewels.pop();
+        auto it = bags.lower_bound(weight);
+        if (it != bags.end()){
+            sum += value;
+            bags.erase(it);
+        }
+    }
+
+    cout << sum;
+    return 0;
+}
