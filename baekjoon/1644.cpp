b5b26ac5426bed2cc412ab3394b6a541f4ffb24b
Youngseok Choe <dev.youngseok@gmail.com>
2021-08-31 23:59:19 +0900

    Solve acmicpc.net 1644 '소수의 연속합'

---
+++ baekjoon/1644.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define faster  ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+int main(){
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#else
+#endif
+    faster
+    int n;
+    scanf("%d", &n);
+    bool sieve[n+1];
+    memset(sieve, true, sizeof(sieve));
+    for (int p=2; p*p <= n; p++){
+        if (sieve[p]){
+            for (int i=p*p; i<=n; i+=p){
+                sieve[i] = false;
+            }
+        }
+    }
+    int sum = 0;
+    int count = 0;
+    queue<int> consective_primes;
+    for (int p=2; p<=n; p++){
+        if (sieve[p]){
+            sum += p;
+            consective_primes.push(p);
+            while (sum > n){
+                sum -= consective_primes.front();
+                consective_primes.pop();
+            }
+            if (sum == n){
+                count++;
+            }
+        }
+    }
+
+    cout << count;
+
+    return 0;
+}
