eae1f3a01ac5a12daa6e2ac1f210d8ac37c99636
Youngseok Choi <youngseok.choi@kaist.ac.kr>
2023-07-23 05:48:37 +0000

    Solve 9252 LCS2

--- baekjoon/9252.cpp
+++ baekjoon/9252.cpp
@@ -1,42 +1,71 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+string s1, s2;
+
+int last_index1[128], last_index2[128];
+int dp[1001][1001];
+// 0: increment first, 1: increment second, 2: increment both
+short decision[1001][1001];
+
+int step(int i, int j){
+    if (i >= s1.length() || j >= s2.length()) return 0;
+    if (dp[i][j] == 0){
+        if (s1[i] == s2[j]){
+            decision[i][j] = 2;
+            dp[i][j] = step(i+1, j+1) + 1;
+        } else {
+            char c1= s1[i];
+            char c2 = s2[j];
+            if (j > last_index2[c1]){
+                decision[i][j] = 0;
+                dp[i][j] = step(i+1, j);
+            }
+            else if (i > last_index1[c2]){
+                decision[i][j] = 1;
+                dp[i][j] = step(i, j+1);
+            }
+            else {
+                int x = step(i+1, j);
+                int y = step(i, j+1);
+                decision[i][j] = (x > y) ? 0 : 1;
+                dp[i][j] = max(x, y);
+            }
+        }
+    }
+    return dp[i][j];
+}
+
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-    string s1,s2;
+    cout.tie(NULL);
     cin >> s1 >> s2;
-    const int a = s1.length(), b = s2.length();
-    int resultArr[a+1][b+1];
-    string resultStringArr[a+1][b+1];
-    for (int i=0;i<=a;i++){
-        for (int j=0;j<=b;j++){
-            resultArr[i][j] = 0;
-            resultStringArr[i][j] = "";
+
+    memset(last_index1, -1, sizeof(last_index1));
+    memset(last_index2, -1, sizeof(last_index2));
+    for (int i=0; i<s1.length(); i++){
+        char c = s1[i];
+        last_index1[c] = max(last_index1[c], i);
+    }
+    for (int i=0; i<s2.length(); i++){
+        char c = s2[i];
+        last_index2[c] = max(last_index2[c], i);
+    }
+
+    int l = step(0, 0);
+    cout << l << '\n';
+    int i = 0, j = 0;
+    while (i < s1.length() && j < s2.length()){
+        if (decision[i][j] == 0){
+            i += 1;
+        } else if (decision[i][j] == 1){
+            j += 1;
+        } else {
+            cout << s1[i];
+            i += 1;
+            j += 1;
         }
     }
-    for (int i=1;i<=a;i++){
-        for (int j=1;j<=b;j++){
-            int k;
-            for (k=i;k>=1;k--){
-                if (s1[k-1] == s2[j-1]) break;
-            }
-            if (k == 0){
-                resultArr[i][j] = resultArr[i][j-1];
-                resultStringArr[i][j] = resultStringArr[i][j-1];
-            }
-            else{
-                if (resultArr[i][j-1] >= resultArr[k-1][j-1]+1){
-                    resultArr[i][j] = resultArr[i][j-1];
-                    resultStringArr[i][j] = resultStringArr[i][j-1];
-                }
-                else{
-                    resultArr[i][j] = resultArr[k-1][j-1]+1;
-                    resultStringArr[i][j] = resultStringArr[k-1][j-1] + s2[j-1];
-                }
-            }
-        }
-    }
-    cout << resultArr[a][b] << endl << resultStringArr[a][b] << endl;
     return 0;
 }
