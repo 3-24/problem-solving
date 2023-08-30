58f6cea642605e4b4158909edc2388ef3be85742
Youngseok <youngseok.choi@kaist.ac.kr>
2023-08-30 14:46:31 +0000

    Solve 16566 "카드 게임"
    #binary-search #lazy-algorithm

--- baekjoon/16566.cpp
+++ baekjoon/16566.cpp
@@ -2,29 +2,51 @@
 
 using namespace std;
 
-multiset<int> L;     // Red-Black Tree
+#define MAX_N 4000000
+
+int parent[MAX_N+1];
+vector<int> arr;
+
+int find(int node){
+    int root = node;
+    while (parent[root] != root){
+        root = parent[root];
+    }
+    parent[node] = root;
+    return root;
+}
 
 int main(){
     ios::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
+
     int n, m, k;
     cin >> n >> m >> k;
 
     int x;
 
     // mlog(m)
-    while (m--){
+    for (int i=0; i<m; i++){
         cin >> x;
-        L.insert(x);
+        arr.push_back(x);
     }
 
-    // k(log(m)+log(m))
+    sort(arr.begin(), arr.end());
+    
+    for (int i=0; i<m; i++){
+        parent[arr[i]] = arr[i];
+    }
+
     while (k--){
         cin >> x;
-        auto it = L.upper_bound(x);
-        cout << *it << '\n';
-        L.erase(it);
+        auto it1 = upper_bound(arr.begin(), arr.end(), x);
+        int root = find(*it1);
+        auto it2 = upper_bound(arr.begin(), arr.end(), root);
+        if (it2 != arr.end()){
+            parent[root] = *it2;
+        }
+        cout << root << '\n';
     }
     return 0;
 }
