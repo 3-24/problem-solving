87b5e388fbb03b90a7fd2d8416d3f58760bd1b32
Youngseok <youngseok.choi@kaist.ac.kr>
2023-10-20 05:00:50 +0000

    Fix test.py to remove trailing whitespaces in each output line

--- test.py
+++ test.py
@@ -14,16 +14,19 @@ with open(f"{problem}.test", 'rb') as f:
     content = f.read()
 
 testcases = content.split(b'\n\n')
+
 for testcase in testcases:
     assert(testcase.startswith(b"IN:\n"))
     out_idx = testcase.find(b"OUT:\n")
     tc_in = testcase[4:out_idx]
-    tc_out = testcase[out_idx+5:]
+    tc_out = testcase[out_idx+5:].strip()
     
     # Feed target with input and check if output is correct
     out = sp.run(problem, input=tc_in, shell=True, stdout=sp.PIPE)
-    if (tc_out.strip() == out.stdout.strip()):
+    
+    actual_out = b'\n'.join(map(lambda x: x.strip(), out.stdout.strip().split(b'\n')))
+    
+    if (tc_out == actual_out):
         print("PASS")
     else:
         print("FAIL")
-    
