ff4f580fdde7bfd161ed729c2c89d0526b072e6a
Youngseok <youngseok.choi@kaist.ac.kr>
2023-10-31 03:56:40 +0000

    Format 3648 "아이돌"

--- baekjoon/3648.cpp
+++ baekjoon/3648.cpp
@@ -10,17 +10,19 @@ int vertexCounter, sccCounter;
 
 stack<int> st;
 
-int do_scc(int u){
+int do_scc(int u) {
     int ret = discovered[u] = ++vertexCounter;
     st.push(u);
 
-    for (int v: graph[u]){
-        if (!discovered[v]) ret = min(ret, do_scc(v));
-        else if (scc[v] == -1) ret = min(ret, discovered[v]);
+    for (int v : graph[u]) {
+        if (!discovered[v])
+            ret = min(ret, do_scc(v));
+        else if (scc[v] == -1)
+            ret = min(ret, discovered[v]);
     }
 
-    if (ret == discovered[u]){
-        while (true){
+    if (ret == discovered[u]) {
+        while (true) {
             int t = st.top();
             st.pop();
             scc[t] = sccCounter;
@@ -32,19 +34,17 @@ int do_scc(int u){
     return ret;
 }
 
-int main(){
-    auto encode = [](int x){
-        return x > 0 ? 2*(x-1) : 2*(-x-1) + 1;
-    };
-    while (cin >> n >> m){
-        graph = vector<vector<int>>(n*2, vector<int>());
-        discovered = vector<int>(n*2, 0);
-        scc = vector<int>(n*2, -1);
+int main() {
+    auto encode = [](int x) { return x > 0 ? 2 * (x - 1) : 2 * (-x - 1) + 1; };
+    while (cin >> n >> m) {
+        graph = vector<vector<int>>(n * 2, vector<int>());
+        discovered = vector<int>(n * 2, 0);
+        scc = vector<int>(n * 2, -1);
         st = stack<int>();
         vertexCounter = 0;
         sccCounter = 0;
 
-        for (int i=0; i<m; i++){
+        for (int i = 0; i < m; i++) {
             int x, y;
             cin >> x >> y;
             x = encode(x);
@@ -55,19 +55,21 @@ int main(){
 
         graph[1].push_back(0);
 
-        for (int i=0; i<2*n; i++){
+        for (int i = 0; i < 2 * n; i++) {
             if (!discovered[i]) do_scc(i);
         }
 
         bool success = true;
-        for (int i=0; i<2*n; i+=2){
-            if (scc[i] == scc[i+1]){
+        for (int i = 0; i < 2 * n; i += 2) {
+            if (scc[i] == scc[i + 1]) {
                 success = false;
                 break;
             }
         }
 
-        if (success) cout << "yes\n";
-        else cout << "no\n";
+        if (success)
+            cout << "yes\n";
+        else
+            cout << "no\n";
     }
 }
