31a558269b327ff396a31c0a8e664376c33c86e3
Youngseok <youngseok.choi@kaist.ac.kr>
2023-11-02 19:15:45 +0000

    Undo verbose

--- test.py
+++ test.py
@@ -3,4 +3,4 @@ from util import MatWaeTeulUnit
 
 if __name__ == '__main__':
     problem = sys.argv[1]
-    exit(MatWaeTeulUnit(problem).test(verbose=True, add_test=True, compile=True))
+    exit(MatWaeTeulUnit(problem).test(verbose=False, add_test=True, compile=True))
